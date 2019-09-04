8b4d98c4152a68c90e749d8466f55f9d96063f77
Nicholas Mota Ferreira <1900953@SALAS.AULAS>
2019-09-03 22:08:09 -0300

    AC02

---
+++ SQL/AC02.sql
@@ -0,0 +1,30 @@
+-- Nicholas Ferreira - 1900953
+-- Leonardo Ferreira - 1901008
+-- João Pedro Bello	 - 1800995
+
+CREATE TABLE PEDIDO(
+	codigo INT IDENTITY,
+	data DATETIME,
+
+	CONSTRAINT pk_pedido PRIMARY KEY (codigo)
+);
+
+CREATE TABLE PRODUTO(
+	codigo INT IDENTITY, 
+	nome VARCHAR(50), 
+	preco SMALLMONEY,
+
+	CONSTRAINT pk_produto PRIMARY KEY (codigo)
+)
+
+CREATE TABLE ITEM(
+	numero INT NOT NULL,
+	cod_pedido INT NOT NULL,
+	cod_produto INT NOT NULL,
+	qtd SMALLINT,
+	preco MONEY,
+
+    CONSTRAINT pk_item PRIMARY KEY (numero),
+    CONSTRAINT fk_item_pedido FOREIGN KEY (cod_pedido) REFERENCES PEDIDO(codigo),
+    CONSTRAINT fk_item_produto FOREIGN KEY (cod_produto) REFERENCES PRODUTO(codigo)
+);
